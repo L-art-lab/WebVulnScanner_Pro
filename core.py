cbce3ac888fe11863797228767fb8de45bc219a2
欣怡 林 <lxy20060916@outlook.com>
2026-08-11 22:10:16 +0800

    feat: 加载完整payloads字典，多线程并发扫描测试通过

--- core.py
+++ core.py
@@ -12,8 +12,8 @@ with open("payloads.txt", "r", encoding="utf-8") as f:
 
 def scan_one_payload(url,payload):
 	try:
-		response = requests.get(url, params=payload,timeout=5)
-		if "SELECT * FROM users" in response.text:
+		response = requests.get(url, params=payload,timeout=15)
+		if "users" in response.text:
 			print("\n[!] 漏洞确认！目标存在SQL注入漏洞。")
 			return{
 			"status": "SQL注入漏洞",
@@ -31,17 +31,33 @@ def scan_one_payload(url,payload):
 	except Exception as e:
 		return None
 def run_scan():
-	url = "http://localhost/test.php"
-	results = []
+    import requests
+    from concurrent.futures import ThreadPoolExecutor
 
-	print(f"[*] 启动多线程扫描，共{len(PAYLOADS)}个任务...")
-
-	with ThreadPoolExecutor(max_workers=5) as executor:
-		future_to_payload = {executor.submit(scan_one_payload,url,p): p
-			for p in PAYLOADS
-		}
-		for future in future_to_payload:
-			result = future.result()
-			if result:
-				results.append(result)
-	return results
+    url = "http://localhost:8000/test.php"
+    PAYLOADS = []
+    
+    # 从 payloads.txt 读取所有攻击语句
+    with open("payloads.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                if "=" in line:
+                    key, value = line.split("=", 1)
+                    PAYLOADS.append({key: value})
+    
+    print(f"[*] 启动多线程扫描，共 {len(PAYLOADS)} 个任务...")
+    results = []
+    
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        future_to_payload = {
+            executor.submit(scan_one_payload, url, p): p 
+            for p in PAYLOADS
+        }
+        
+        for future in future_to_payload:
+            result = future.result()
+            if result:
+                results.append(result)
+    
+    return results
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$id = $_GET['id'];
+echo "SELECT * FROM users WHERE id = '$id'";
+?>
